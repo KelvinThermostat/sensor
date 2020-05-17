8b21e8d5caabd5f27828c6a705e87abad37c6996
Ismael Monteiro <ismaelmds@outlook.com>
2020-05-17 19:39:20 +0100

    Sensor readings implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,118 @@
 #include <Arduino.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
 #include <ESP8266WebServer.h>
+#include <BME280I2C.h>
 #include <Wire.h>
 
 float temperature, humidity, pressure;
 const char *ssid = "BaconNet";
 const char *password = "5AlandaleClose//";
 
-Adafruit_BME280 bme;
+BME280I2C::Settings settings(
+    BME280::OSR_X1,
+    BME280::OSR_X1,
+    BME280::OSR_X1,
+    BME280::Mode_Forced,
+    BME280::StandbyTime_1000ms,
+    BME280::Filter_Off,
+    BME280::SpiEnable_False,
+    BME280I2C::I2CAddr_0x76 // I2C address. I2C specific.
+);
+
+BME280I2C bme(settings);
+
 ESP8266WebServer server(80);
 
 String sendData(float temperature, float humidity, float pressure)
 {
-  String ptr = "{\n";
-  ptr += "\"temperature\": ";
-  ptr += temperature;
-  ptr += ",\n";
-  ptr += "\"humidity\": ";
-  ptr += humidity;
-  ptr += ",\n";
-  ptr += "\"pressure\": ";
-  ptr += pressure;
-  ptr += "\n}";
+    String ptr = "{\n";
+    ptr += "\"temperature\": ";
+    ptr += temperature;
+    ptr += ",\n";
+    ptr += "\"humidity\": ";
+    ptr += humidity;
+    ptr += ",\n";
+    ptr += "\"pressure\": ";
+    ptr += pressure;
+    ptr += "\n}";
 
-  return ptr;
+    return ptr;
 }
 
 void handle_OnConnect()
 {
-  temperature = bme.readTemperature();
-  humidity = bme.readHumidity();
-  pressure = bme.readPressure() / 100.0F;
+    float temp(NAN), hum(NAN), pres(NAN);
 
-  server.send(200, "application/json", sendData(temperature, humidity, pressure));
+    BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+    BME280::PresUnit presUnit(BME280::PresUnit_hPa);
+
+    bme.read(pres, temp, hum, tempUnit, presUnit);
+
+    server.send(200, "application/json", sendData(temp, hum, pres));
 }
 
 void handle_NotFound()
 {
-  server.send(404, "text/plain", "Not found");
+    server.send(404, "text/plain", "Not found");
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  delay(100);
+    Serial.begin(9600);
+    delay(100);
 
-  bme.begin(0x76);
+    while (!Serial)
+    {
+    } // Wait
 
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
+    Wire.begin();
+    while (!bme.begin())
+    {
+        Serial.println("Could not find BME280I2C sensor!");
+        delay(1000);
+    }
 
-  //connect to your local wi-fi network
-  WiFi.begin(ssid, password);
+    switch (bme.chipModel())
+    {
+    case BME280::ChipModel_BME280:
+        Serial.println("Found BME280 sensor! Success.");
+        break;
+    case BME280::ChipModel_BMP280:
+        Serial.println("Found BMP280 sensor! No Humidity available.");
+        break;
+    default:
+        Serial.println("Found UNKNOWN sensor! Error!");
+    }
 
-  //check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");
-  Serial.println(WiFi.localIP());
+    // Change some settings before using.
+    settings.tempOSR = BME280::OSR_X4;
 
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
+    bme.setSettings(settings);
 
-  server.begin();
-  Serial.println("HTTP server started");
+    Serial.println("Connecting to ");
+    Serial.println(ssid);
+
+    //connect to your local wi-fi network
+    WiFi.begin(ssid, password);
+
+    //check wi-fi is connected to wi-fi network
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected..!");
+    Serial.print("Got IP: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", handle_OnConnect);
+    server.onNotFound(handle_NotFound);
+
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
 void loop()
 {
-  server.handleClient();
+    server.handleClient();
 }
